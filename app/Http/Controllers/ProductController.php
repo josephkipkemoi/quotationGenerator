704b21d2c265abbc288e34020521f7fbcfe7d632
jkemboe <jkemboe@gmail.com>
2021-11-03 15:27:19 -0400

    Fixed minor bug

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
-use App\Models\Quotation;
-use App\Models\User;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -78,6 +76,6 @@ class ProductController extends Controller
     public function destroy($id)
     {
         //
-        Product::find($id)->truncate();
+        Product::find($id)->delete();
     }
 }
